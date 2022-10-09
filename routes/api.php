90d1103061f0f3579b646a98be811d819cff7773
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 22:31:31 +0200

    Refactored `api`

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
