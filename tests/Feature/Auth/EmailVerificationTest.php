89103d7390e57a30d1a2934687b660aa89d35dd3
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 14:07:26 +0200

    Fix `EmailVerificationTest`

--- tests/Feature/Auth/EmailVerificationTest.php
+++ tests/Feature/Auth/EmailVerificationTest.php
@@ -10,6 +10,7 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\URL;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class EmailVerificationTest extends TestCase
@@ -30,11 +31,15 @@ class EmailVerificationTest extends TestCase
             ['id' => $user->id, 'hash' => sha1($user->email)]
         );
 
-        $response = $this->actingAs($user)->get($verificationUrl);
+        Sanctum::actingAs($user);
+
+        $response = $this->get($verificationUrl);
 
         Event::assertDispatched(Verified::class);
         $this->assertTrue($user->fresh()->hasVerifiedEmail());
-        $response->assertRedirect(config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1');
+        $response->assertRedirect(
+            config('app.frontend_url').RouteServiceProvider::HOME.'?verified=1'
+        );
     }
 
     public function test_email_is_not_verified_with_invalid_hash()
@@ -49,7 +54,9 @@ class EmailVerificationTest extends TestCase
             ['id' => $user->id, 'hash' => sha1('wrong-email')]
         );
 
-        $this->actingAs($user)->get($verificationUrl);
+        Sanctum::actingAs($user);
+    
+        $this->get($verificationUrl);
 
         $this->assertFalse($user->fresh()->hasVerifiedEmail());
     }
