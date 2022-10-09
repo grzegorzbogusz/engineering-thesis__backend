58019401537d4e0e3145ef4c7ece9be615f9499d
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 23:34:08 +0200

    Added `isAdmin` method

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function isAdmin(): bool
+    {
+        return (bool) $this->is_admin;
+    }
 }
