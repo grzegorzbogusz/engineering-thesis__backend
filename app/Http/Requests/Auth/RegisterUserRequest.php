379406fe550d5000a376f8e3f6af35a844a4b5a5
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 10:58:16 +0200

    Rename FormRequest

--- app/Http/Requests/Auth/RegisterUserRequest.php
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -7,7 +7,7 @@ namespace App\Http\Requests\Auth;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rules;
 
-class RegisteredUserRequest extends FormRequest
+class RegisterUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
