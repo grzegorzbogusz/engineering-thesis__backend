3d0ffb9ffcda41875e623a5e3236493cb00b03d5
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 11:52:55 +0200

    Changed `LogoutController` logic

--- app/Http/Controllers/Auth/LogoutController.php
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -5,13 +5,18 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Services\Auth\UserLogout;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class LogoutController extends Controller
 {
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, UserLogout $service): JsonResponse
     {
-        //
+        $token = $request->user()->currentAccessToken();
+
+        $service->logout($token);
+
+        return new JsonResponse();
     }
 }
