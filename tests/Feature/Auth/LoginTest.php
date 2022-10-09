6faf9da7785099c35c95dcc370f7914414a6ef7c
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 12:21:52 +0200

    Rename `AuthenticationTest` to `LoginTest`

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -8,28 +8,28 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class AuthenticationTest extends TestCase
+class LoginTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_users_can_authenticate_using_the_login_screen()
+    public function test_users_can_login_using_the_login_screen()
     {
         $user = User::factory()->create();
 
-        $response = $this->post('/login', [
+        $response = $this->post(route('login'), [
             'email' => $user->email,
             'password' => 'password',
         ]);
 
-        $this->assertAuthenticated();
+        $this->assertAuthenticated('auth:sanctum');
         $response->assertNoContent();
     }
 
-    public function test_users_can_not_authenticate_with_invalid_password()
+    public function test_users_can_not_login_with_invalid_password()
     {
         $user = User::factory()->create();
 
-        $this->post('/login', [
+        $this->post(route('login'), [
             'email' => $user->email,
             'password' => 'wrong-password',
         ]);
