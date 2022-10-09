53e0061878f379b299467f0330aa4e6069c6cea6
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-09 11:34:27 +0200

    Changed `RegisterUserController` logic

--- app/Http/Controllers/Auth/RegisterUserController.php
+++ app/Http/Controllers/Auth/RegisterUserController.php
@@ -6,26 +6,25 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\RegisterUserRequest;
-use App\Models\User;
+use App\Services\UserRegistrar;
 use Illuminate\Auth\Events\Registered;
-use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterUserController extends Controller
 {
-    public function store(RegisterUserRequest $request): Response
+    public function __invoke(RegisterUserRequest $request, UserRegistrar $service): JsonResponse
     {
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = $service->register($validated);
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return response()->noContent();
+        return response()->json(status: 201);
     }
 }
