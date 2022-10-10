134094e088d95aac21b31542b4c2582ef727af89
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-10 21:16:40 +0200

    Created `UserManagementController` with logic

--- app/Http/Controllers/Admin/UserManagementController.php
+++ app/Http/Controllers/Admin/UserManagementController.php
@@ -7,23 +7,30 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use App\Services\Account\AccountDeleter;
+use App\Services\Admin\UsersRetriever;
 use Illuminate\Http\JsonResponse;
 
 class UserManagementController extends Controller
 {
     public function index(): JsonResponse
     {
-        return response('Users here');
+        $users = UsersRetriever::get();
+        
+        return response()->json($users);
     }
 
     public function show(User $user): JsonResponse
     {
-        
+        if ($user->isAdmin()) {
+            abort(400, 'Admin account can not be selected');
+        }
+
+        return response()->json($user);
     }
 
-    public function destroy(User $user, AccountDeleter $service): JsonResponse
+    public function destroy(User $user): JsonResponse
     {
-        $service->delete($user);
+        AccountDeleter::delete($user);
 
         return response()->json(['message' => 'Account deleted'], 200);
     }
