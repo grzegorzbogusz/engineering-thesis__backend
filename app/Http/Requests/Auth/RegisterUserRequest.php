27542443175a8052f2b4ff7f22147abdd6c3a68d
Grzegorz Bogusz <gbogusz55@gmail.com>
2022-10-18 13:55:41 +0200

    Fixed `RegisterUserRequest` rules

--- app/Http/Requests/Auth/RegisterUserRequest.php
+++ app/Http/Requests/Auth/RegisterUserRequest.php
@@ -26,7 +26,7 @@ class RegisterUserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:20', 'min:4'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'confirmed', 'min:8'],
         ];
